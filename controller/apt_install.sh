18c7a940edb3adc135e3dd479505be68295595b8
yhb <echoyhb@163.com>
2022-02-12 10:55:49 +0800

    实现交互自动化，将keystone_install.sh合入ops_install.sh

--- controller/apt_install.sh
+++ controller/apt_install.sh
@@ -6,7 +6,7 @@ add-apt-repository cloud-archive:stein -y
 apt-key adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.asc'
 add-apt-repository 'deb [arch=amd64,arm64,ppc64el] https://mirrors.aliyun.com/mariadb/repo/10.4/ubuntu bionic main'
 apt update
-apt install -y  python-pip python3-pip python python3 git \
+apt install -y  python-pip python3-pip python python3 git expect\
 	chrony python3-openstackclient mariadb-server mariadb-client python-pymysql \
 	rabbitmq-server memcached python3-memcache etcd keystone glance placement-api \
 	nova-api nova-conductor nova-consoleauth \
--- controller/ops_install.sh
+++ controller/ops_install.sh
@@ -4,6 +4,133 @@
 source ops_env.sh
 source make_config.sh
 
+# 配置 chrony
+echo "配置 chrony"
+
+cp /etc/chrony/chrony.conf /etc/chrony/chrony.conf.bak
+cp "${CRTDIR}/config/chrony.conf" /etc/chrony/chrony.conf
+
+service chrony restart
+chronyc sources
+
+# 配置 maridb
+echo "配置 maridb"
+
+cp "${CRTDIR}/config/99-openstack.cnf" /etc/mysql/mariadb.conf.d/99-openstack.cnf
+
+service mysql restart
+
+mysql_secure_installation <<EOF
+
+n
+y
+${pwd_mysql}
+${pwd_mysql}
+n
+n
+n
+y
+EOF
+
+# 配置rabbitmq
+echo "配置rabbitmq"
+
+rabbitmqctl add_user openstack ${pwd_ops}
+rabbitmqctl set_permissions openstack ".*" ".*" ".*"
+
+service rabbitmq-server restart
+
+# 配置 memcached
+echo "配置 memcached"
+
+cp /etc/memcached.conf /etc/memcached.conf.bak
+cp "${CRTDIR}/config/memcached.conf" /etc/memcached.conf
+
+service memcached restart
+
+# 配置 etcd
+echo "配置 etcd"
+
+cp /etc/default/etcd /etc/default/etcd.bak
+cp "${CRTDIR}/config/etcd" /etc/default/etcd
+
+systemctl enable etcd
+systemctl restart etcd
+
+# 配置 keystone
+echo "配置 keystone"
+
+mysql <<EOF
+CREATE DATABASE keystone;
+GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'localhost' \
+IDENTIFIED BY '${pwd_ops}';
+GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'%' \
+IDENTIFIED BY '${pwd_ops}';
+EOF
+
+cp /etc/keystone/keystone.conf /etc/keystone/keystone.conf.bak
+cp "${CRTDIR}/config/keystone.conf" /etc/keystone/keystone.conf
+
+su -s /bin/sh -c "keystone-manage db_sync" keystone
+keystone-manage fernet_setup --keystone-user keystone --keystone-group keystone
+keystone-manage credential_setup --keystone-user keystone --keystone-group keystone
+keystone-manage bootstrap --bootstrap-password "${pwd_ops}" \
+  --bootstrap-admin-url http://controller:5000/v3/ \
+  --bootstrap-internal-url http://controller:5000/v3/ \
+  --bootstrap-public-url http://controller:5000/v3/ \
+  --bootstrap-region-id RegionOne
+
+cp /etc/apache2/apache2.conf /etc/apache2/apache2.conf.bak
+cp "${CRTDIR}/config/apache2.conf" /etc/apache2/apache2.conf
+
+service apache2 restart
+
+
+source "${CRTDIR}/config/admin-openrc"
+
+openstack project create --domain default \
+  --description "Service Project" service
+
+
+# # 添加openstack用户
+source "${CRTDIR}/config/admin-openrc"
+expect <<EOF
+spawn openstack user create --domain default --password-prompt glance
+expect "*Password*" { send "${pwd_ops}\r" }
+expect "*Repeat*" { send "${pwd_ops}\r" }
+expect eof
+EOF
+expect <<EOF
+spawn openstack user create --domain default --password-prompt placement
+expect "*Password*" { send "${pwd_ops}\r" }
+expect "*Repeat*" { send "${pwd_ops}\r" }
+expect eof
+EOF
+expect <<EOF
+spawn openstack user create --domain default --password-prompt nova
+expect "*Password*" { send "${pwd_ops}\r" }
+expect "*Repeat*" { send "${pwd_ops}\r" }
+expect eof
+EOF
+expect <<EOF
+spawn openstack user create --domain default --password-prompt neutron
+expect "*Password*" { send "${pwd_ops}\r" }
+expect "*Repeat*" { send "${pwd_ops}\r" }
+expect eof
+EOF
+expect <<EOF
+spawn openstack user create --domain default --password-prompt zun
+expect "*Password*" { send "${pwd_ops}\r" }
+expect "*Repeat*" { send "${pwd_ops}\r" }
+expect eof
+EOF
+expect <<EOF
+spawn openstack user create --domain default --password-prompt cinder
+expect "*Password*" { send "${pwd_ops}\r" }
+expect "*Repeat*" { send "${pwd_ops}\r" }
+expect eof
+EOF
+
 # 配置 glance 
 echo "配置 glance "
 
