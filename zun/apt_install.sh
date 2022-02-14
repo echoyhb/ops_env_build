d13f3ec6ca13cd4148553d5fe664dd39f3bde581
yhb <echoyhb@163.com>
2022-02-14 22:43:44 +0800

    fix

--- zun/apt_install.sh
+++ zun/apt_install.sh
@@ -3,6 +3,7 @@ apt update && apt upgrade -y
 apt install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://mirrors.aliyun.com/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
+add-apt-repository cloud-archive:stein -y
 apt update
 apt install -y python-pip python3-pip python python3 git expect chrony docker-ce \
 	docker-ce-cli containerd.io numactl
